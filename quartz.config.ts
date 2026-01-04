d6b614f2c0d377b8f50667c1520bec5c888e6b20
dcrobot <ehrudxo@gmail.com>
2026-01-04 16:19:45 +0900

    og image added

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dcrobot-keen.github.io/modern-robot-sw",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
